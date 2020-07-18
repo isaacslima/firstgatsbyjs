d1080fcc7f6d775b96d4ca0fd3ba2e00303f2b3c
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-07-18 14:55:09 -0300

    add user on about

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,15 +2,28 @@ import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
 import Container from "../components/container"
+import styles from "./about.module.css"
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
 
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
       <Link to="/">Home</Link>
-      <Header headerText="About Gatsby" />
-      <Container>
-        <p>Such wow. Very React.</p>
-      </Container>
+      <Header headerText="About" />
+      <User
+        username="Isaac Sousa"
+        avatar="https://avatars3.githubusercontent.com/u/11709857?s=400&u=0dda473685553140c02cae89e3bfac71b11b4b44&v=4"
+        excerpt="I'm a software engineer."
+      />
     </div>
   )
 }
