23e49e11a4bab566635f7e09092ec333ed716c89
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-07-18 13:23:39 -0300

    add contact page and link to

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "./components/header"
+export default function Contact() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Link to="/">Home</Link>
+      <Header headerText="Contact" />
+      <p>my Contacts</p>
+      <p><a target="_blank" href="https://github.com/isaacslima">GitHub</a></p>
+    </div>
+  )
+}
